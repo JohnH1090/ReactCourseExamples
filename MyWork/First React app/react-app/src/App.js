5424bd905efef3e511df9fc0b7d21cbfecd02a97
John Horgan <johnhorgan12@hotmail.com>
2024-02-18 18:06:20 +0000

    first app use state

--- MyWork/First React app/react-app/src/App.js
+++ MyWork/First React app/react-app/src/App.js
@@ -1,14 +1,31 @@
 //import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
- // console.log(what);
+  const [emotion, setEmotion] = useState("Happy");
+  const [secondary, setSecondary] = useState("Tired");
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here`);
+  }, [secondary])
+ 
   return (
     <div className="App">
+
+
       <h1>Current Emotion is {emotion} </h1>
+      <button onClick={() => setEmotion("Sad") }>Sad</button>
+      <button onClick={() => setEmotion("Excited") }>Excited</button>
+      <button onClick={() => setEmotion("Happy") }>Happy</button>
+
+      <h2>Current Secondary Emotion is {secondary} </h2>
+      <button onClick={() => setSecondary("Greatful") }>Greatful</button>
+      <button onClick={() => setSecondary("Tired") }>Tired</button>
     </div>
   );
 }
