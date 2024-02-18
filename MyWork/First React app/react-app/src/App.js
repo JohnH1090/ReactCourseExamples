efed2701ff488968ad0b18505f2cb67ae7535b3d
John Horgan <johnhorgan12@hotmail.com>
2024-02-18 18:25:22 +0000

    useReducer

--- MyWork/First React app/react-app/src/App.js
+++ MyWork/First React app/react-app/src/App.js
@@ -1,31 +1,19 @@
 //import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from 'react';
+import {useReducer} from 'react';
 
 
 function App() {
-  const [emotion, setEmotion] = useState("Happy");
-  const [secondary, setSecondary] = useState("Tired");
-  useEffect(() => {
-    console.log(`It's ${emotion} right now`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here`);
-  }, [secondary])
+  const [checked, setChecked] = useReducer(checked => !checked, false );
+ 
+  
  
   return (
     <div className="App">
 
-
-      <h1>Current Emotion is {emotion} </h1>
-      <button onClick={() => setEmotion("Sad") }>Sad</button>
-      <button onClick={() => setEmotion("Excited") }>Excited</button>
-      <button onClick={() => setEmotion("Happy") }>Happy</button>
-
-      <h2>Current Secondary Emotion is {secondary} </h2>
-      <button onClick={() => setSecondary("Greatful") }>Greatful</button>
-      <button onClick={() => setSecondary("Tired") }>Tired</button>
+      <input type="checkbox" value={checked} onChange={setChecked} />
+      <label>{ checked ? "Checked" : "Not Checked"}</label>
+      
     </div>
   );
 }
