b2ab41377931aa2e749ca4c0477ec6ca571e65aa
John Horgan <johnhorgan12@hotmail.com>
2024-03-01 14:30:45 +0000

    finished Routes example

--- MyWork/First React app/react-app/src/App.js
+++ MyWork/First React app/react-app/src/App.js
@@ -1,9 +1,15 @@
 //import logo from './logo.svg';
 import "./App.css";
+import {Link, Outlet} from "react-router-dom";
 
 function Home() {
   return (
     <div>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact Us</Link>
+        
+      </nav>
       <h1>My Website</h1>
     </div>
   );
@@ -12,14 +18,37 @@ function Home() {
 export function About() {
   return (
     <div>
+      
+      <nav>
+      <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact Us</Link>
+        
+      </nav>
       <h1>About Us</h1>
+      <Outlet />
     </div>
   );
+}
+
+export function History() {
+  return (
+    <div>
+      <h1>Our History</h1>
+    </div>
+  )
+
 }
 
 export function Contact() {
   return (
     <div>
+      <nav>
+      <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact Us</Link>
+        
+      </nav>
       <h1>Contact us</h1>
     </div>
   );
